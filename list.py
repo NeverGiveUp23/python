d5e8836cf2aa10cc18fd9f7e06cdd27e5e48669e
NeverGiveUp23 <felixvjr0@gmail.com>
2022-10-22 15:37:23 -0400

    Saturday work

---
+++ list.py
@@ -0,0 +1,21 @@
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+salad = 3 * vegetables
+print(salad)
+
+# above is adding list together and also printing it multible times to the console on line 5 #
+
+words = ["start","going","till","the","end"]
+# Sub-ranges (portions) of the list
+print(words[1:]) # prints ['going', 'till', 'the', 'end']
+print(words[:4]) # prints ['start', 'going', 'till', 'the']
+print(words[2:4]) # prints ['till', 'the']
+    
+# Making a copy of a list
+copy_of_words = words[:]
+copy_of_words.append("dojo") # only appends to the copy
+print(copy_of_words) # prints ['start', 'going', 'till', 'the', 'end', 'dojo']
+print(words) # prints ['start', 'going', 'till', 'the', 'end']
+
