ed113aca0c0d57d8ab4a98ee886ef99d7e68cd9b
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2022-10-22 14:50:00 -0400

    Create hellos_world.py

---
+++ hellos_world.py
@@ -0,0 +1,7 @@
+print('Hello World')
+name = 'Felix Vargas Jr'
+print(f"Hello {name}")
+num = 23
+print(f'Hello {num}')
+fav_foods = ('pizza', 'pasta')
+print("I love to eat {} and {}".format(fav_foods[0],fav_foods[1]))
