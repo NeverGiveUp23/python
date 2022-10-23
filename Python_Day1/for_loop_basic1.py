f51e648a4ebc70bbb627e643a6fd8347dd219293
NeverGiveUp23 <felixvjr0@gmail.com>
2022-10-23 10:38:07 -0400

    function

--- Python_Day1/for_loop_basic1.py
+++ Python_Day1/for_loop_basic1.py
@@ -1,18 +1,18 @@
-# for x in range(0,150):
-#   print(x)
+for x in range(0,150):
+  print(x)
 
-# for i in range(5, 1000,5):
-#   print(i)
+for i in range(5, 1000,5):
+  print(i)
 
-# for i in range(1, 100):
-#   print(i)
-#   if(i % 5):
-#     print("coding")
-#   elif(i % 10):
-#     print('Coding Dojo')
+for i in range(1, 100):
+  print(i)
+  if(i % 5):
+    print("coding")
+  elif(i % 10):
+    print('Coding Dojo')
 
-# for i in range(2018,0,-4):
-#   print(i)
+for i in range(2018,0,-4):
+  print(i)
 
 def add(a, b):
     x = a + b
