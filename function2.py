0966f3019ae5499dd344f85f44422b20cc7cffaa
NeverGiveUp23 <felixvjr0@gmail.com>
2022-10-23 11:23:31 -0400

    function2

---
+++ function2.py
@@ -0,0 +1,6 @@
+def countdown(num):
+  for i in range(num, 0, -1):
+    print (i)
+countdown(10)
+#this will create a countdown/ create a function with a parameter and than create the for loop with the parameter as the number/ so that way when you put any number it will decrease by 1
+
