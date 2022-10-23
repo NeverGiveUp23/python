49a8c79a624e31fd2d8258fcc5b1527bb0eaccd9
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2022-10-23 10:38:44 -0400

    Update function.py

--- function.py
+++ function.py
@@ -1,36 +1,36 @@
-# def be_cheerful(name='Vicky',repeat=2):
-#   print(f"GoodMorning {name}\n"* repeat)
+def be_cheerful(name='Vicky',repeat=2):
+  print(f"GoodMorning {name}\n"* repeat)
   
-# be_cheerful("Felix")
-# be_cheerful()
-# be_cheerful("Benny Bob", 10)
+be_cheerful("Felix")
+be_cheerful()
+be_cheerful("Benny Bob", 10)
 
-#1
-# def number_of_food_groups():
-#     return 5
-# print(number_of_food_groups())
-# this returned just the number 5 because that is what is being called in the function
+1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+this returned just the number 5 because that is what is being called in the function
 
 
-#3
+3
 
-# def number_of_books_on_hold():
-#     return 5
-#     return 10
-# print(number_of_books_on_hold())
-#it is returning 5 only due to it being the first before "return 10", the code will never see that once a return call is there
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+it is returning 5 only due to it being the first before "return 10", the code will never see that once a return call is there
 
-#4
-# def number_of_fingers():
-#     return 5
-#     print(10)
-# print(number_of_fingers())
-#i thought it would still print out 10, i was wrong it would only still return 5
+4
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+i thought it would still print out 10, i was wrong it would only still return 5
 
-# #6
-# def add(b,c):
-#     print(b+c)
-# print(add(1,2) + add(2,3))
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
 
 
 # #7
@@ -39,5 +39,5 @@
 #     print(concatenate(2,5))
 
 
-#h
+
 
