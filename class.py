2797ad6d1f207f3a85a538376b5df165a3d51140
NeverGiveUp23 <felixvjr0@gmail.com>
2022-10-25 16:43:07 -0400

    classes OOP

--- class.py
+++ class.py
@@ -62,48 +62,50 @@
 
 
 
-# class BankAccount:
-#     # don't forget to add some default values for these parameters!
-#     def __init__(self, int_rate, balance): 
-#       self.int_rate = int_rate
-#       self.balance = balance
-#         # don't worry about user info here; we'll involve the User class soon
-#     def deposit(self, amount):
-#       self.new_balance = self.balance + amount
-#       print(f"your balance is now {self.new_balance}")
-#       return self
-#     # above i setting the deposit in for the user, it is adding what the user already has in the bank + what they're about to deposit and printing it to the console
-#     def withdraw(self, amount):
-#       self.new_balance2 = self.balance - amount
-#       if self.balance == 0:
-#         self.balance2 = self.balance - 5
-#         print(f"Your Balance is now {self.balance2}")
-#       else:
-#         print(f"Your balance is now {self.new_balance2}")
-#     # above is just creating a new balance in balance2 and setting it to the balance and - the amount that is being put in. also setting an if statement for if the user has a low balance
-#     def display_account_info(self):
-#       print(f"Balance: {self.balance}")
-#       return self
-#     def yield_interest(self):
-#       self.balance = self.balance * self.int_rate
-#       print(f"you gained {self.balance}")
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+      self.int_rate = int_rate
+      self.balance = balance
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+      self.new_balance = self.balance + amount
+      print(f"your balance is now {self.new_balance}")
+      return self
+    # above i setting the deposit in for the user, it is adding what the user already has in the bank + what they're about to deposit and printing it to the console
+    def withdraw(self, amount):
+      self.new_balance2 = self.balance - amount
+      if self.balance == 0:
+        self.balance2 = self.balance - 5
+        print(f"Your Balance is now {self.balance2}")
+      else:
+        print(f"Your balance is now {self.new_balance2}")
+    # above is just creating a new balance in balance2 and setting it to the balance and - the amount that is being put in. also setting an if statement for if the user has a low balance
+    def display_account_info(self):
+      print(f"Balance: {self.balance}")
+      return self
+    def yield_interest(self):
+      self.balance = self.balance * self.int_rate
+      print(f"you gained {self.balance}")
     
     
-# bank_info = BankAccount(0.01, 2000)
-# bank_info.deposit(200).deposit(300).withdraw(100)
+bank_info = BankAccount(0.01, 2000)
+bank_info.deposit(200).deposit(300).withdraw(100)
 
 class User:
   def __init__(self, first_name, last_name):
     self.first_name = first_name
     self.last_name = last_name
-    self.account = BankAccount(int_rate=0.03, balance=600)
+    self.account = BankAccount(int_rate=0.03, balance = 100)
     
   def make_deposit(self, amount):
     self.account.deposit(amount)
+    return self
     
     
   def display(self):
     print(f"Felix, Checking Balance: {self.account.balance}")
+    return self
     
 
 
@@ -112,4 +114,4 @@ class User:
 
 
 display_user_balance = User('Felix', 'Vargas')
-display_user_balance.make_deposit(100)
+display_user_balance.make_deposit(300).display()
--- class2.py
+++ class2.py
@@ -1,4 +1,10 @@
+
+
+
 class Person:
+  
+  int_rate = 0.5
+    
   def __init__(self, money, name, deposit, paycheck):
     self.money = money
     self.name = name
@@ -8,20 +14,29 @@ class Person:
   def money_deposit(self):
     self.deposit = self.money + self.deposit
     print(f"Hello {self.name} here is your daily Balance: {self.deposit}")
+    return self
     
   def paid(self):
     self.money += self.paycheck
     print(f"your paycheck is {self.paycheck}")
+    tax = self.paycheck*Person.tax_rate
+    self.paycheck += self.paycheck - tax
+    print(f"We will take out {tax} for taxes! You have {self.money} in your account")
+    return self
   
-felix = Person(1000, "Felix", 9000, 400)
+  @classmethod
+  def change_tax_rate(cls, new_rate):
+    cls.tax_rate = new_rate
+  
+felix = Person(2000, "Felix", 9000, 1000)
 john_doe = Person(30, "John", 400, 500)
-
+Person.change_tax_rate(0.1)
 print(f"${felix.money} is what I have and my name is {felix.name} and I deposited {felix.deposit}")
-felix.money_deposit()
+felix.money_deposit().paid()
 john_doe.money_deposit()
-felix.paid()
 
 
+# work on this more and practice classes
 
 
 
