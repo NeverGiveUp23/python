7adf466fa6ceb78bb2b2ddb8dfa82882098aa84f
NeverGiveUp23 <felixvjr0@gmail.com>
2022-10-24 20:04:23 -0400

    OOP

--- class.py
+++ class.py
@@ -1,38 +1,38 @@
 # class User:
-  def __init__(self):
-    self.first_name = "Felix"
-    self.last_name = 'Vargas'
-    self.age = 29
+#   def __init__(self):
+#     self.first_name = "Felix"
+#     self.last_name = 'Vargas'
+#     self.age = 29
 
-print("Hello")
-user_felix = User()
-print(user_felix.first_name)
+# print("Hello")
+# user_felix = User()
+# print(user_felix.first_name)
 
-user_2 = User()
-print(user_2.first_name)
+# user_2 = User()
+# print(user_2.first_name)
 
 # adjusting user classes
 
-class Shoe:
-  def __init__(self, brand, shoe_type, price):
-    self.brand = brand
-    self.type = shoe_type
-    self.price = price
-    self.in_stock = True
-skater_shoe = Shoe("Nike", "Air-Force", 79.99)
-print(skater_shoe.brand)
+# class Shoe:
+#   def __init__(self, brand, shoe_type, price):
+#     self.brand = brand
+#     self.type = shoe_type
+#     self.price = price
+#     self.in_stock = True
+# skater_shoe = Shoe("Nike", "Air-Force", 79.99)
+# print(skater_shoe.brand)
 
 #above will make a class with the description of the items in the init method, and than assigning it to each other and after creating a shoe.
 
-class User:
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
-  def greeting(self):
-    print(f"Hello, my name is {self.name}")
+# class User:
+#   def __init__(self, name, email):
+#     self.name = name
+#     self.email = email
+#   def greeting(self):
+#     print(f"Hello, my name is {self.name}")
     
-felix = User("Felix", "felixvjr0@gmail.com")
-felix.greeting()
+# felix = User("Felix", "felixvjr0@gmail.com")
+# felix.greeting()
 
 # this will get us to print out the user's name, assign the parameters the its on self.name and create a function that will put it all together for the user.
 #create a new variable to assign it the User class with the parameters describing the user.
@@ -56,6 +56,9 @@ felix.greeting()
 # user_info.enroll().display()
 
 # above is a way to adjust classes and th user by being a member and listing out their info
+
+
+
 
 
 
@@ -86,5 +89,23 @@ class BankAccount:
       print(f"you gained {self.balance}")
     
     
-bank_info = BankAccount(0.01, 2000)
-bank_info.deposit(200).deposit(300).withdraw(100)
+# bank_info = BankAccount(0.01, 2000)
+# bank_info.deposit(200).deposit(300).withdraw(100)
+
+class User:
+  def __init__(self, first_name, last_name):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.account = BankAccount(int_rate=0.03, balance=600)
+    
+  def make_deposit(self, amount):
+    self.account.deposit(amount)
+    
+    
+  def display(self):
+    print(f"Felix, Checking Balance: {self.account.balance}")
+    
+
+    
+display_user_balance = User('Felix', 'Vargas')
+display_user_balance.make_deposit(100)
